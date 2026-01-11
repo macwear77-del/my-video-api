91e2ba135a4915cba24f7f576d4c4540d5fa1630
macwear77-del <macwear77@gmail.com>
2026-01-11 12:49:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "Server is Live"}
+    return {"message": "Server is Live"}
 
 @app.get("/process")
-def process_video(video_url: str):
+def process(video_url: str):
     try:
         ydl_opts = {'format': 'best'}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
-            download_url = info.get('url')
-        return RedirectResponse(url=download_url)
+            video_link = info.get('url')
+        return RedirectResponse(url=video_link)
     except Exception as e:
         return {"error": str(e)}
